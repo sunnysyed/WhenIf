5f65bbac458fcd8e7252fc12558396a834277cc8
Austin Carter <gameboy2m@yahoo.com>
2017-05-10 01:55:18 -0500

    Scraper Update and Cataclysm
    -Degrees now store Concentrations by index instead of storing the
    Concentration objects themselves. This is for database compatability.
    -Concentrations now store their Classes in the same fassion as above.
    -Added 'OR' variable to Class Objects to handle when there is an
    either/or Class path option.
    -Wrote most of the regular expressions to succesfully parse the
    PREREQUISTES for applicable classes.
    -While finishing 'get_prereqs' an update on the DePaul website was
    pushed through. This resulted in some of the urls being changed, which I
    fixed, but it also seems the data is no longer available in a json
    format and the scraper now gives a 'No JSON object could be decoded'
    error.

--- modules/urlscrape.py
+++ modules/urlscrape.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 import time
 import urllib
 import json
+from collections import OrderedDict
 
 # - - - - - CLASSES - - - - - #
 
@@ -23,7 +24,7 @@ class Concentration:
         self.classes = classes
 
 class Class:
-    def __init__(self, code, name, isIntro, prereqs, terms, inClassOnly, onlineOnly, priority):
+    def __init__(self, code, name, isIntro, prereqs, terms, inClassOnly, onlineOnly, priority, OR):
         self.code = code
         self.name = name
         self.isIntro = isIntro
@@ -32,6 +33,7 @@ class Class:
         self.inClassOnly = inClassOnly
         self.onlineOnly = onlineOnly
         self.priority = priority
+        self.OR = OR
 
 # - - - - - DATA LINKS - - - - - #
 
@@ -39,30 +41,29 @@ base = "http://www.cdm.depaul.edu"
 
 classLinks = {}
 
-classDict = {}
+concentrationDict = OrderedDict()
 
-classLinks['IS'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27IS%27&$format=json"
+classDict = OrderedDict()
 
-classLinks['CSC'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27CSC%27&$format=json"
+classLinks['IS'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27IS%27&$expression=json"
 
-classLinks['ECT'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27ECT%27&$format=json"
+classLinks['CSC'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27CSC%27&$expression=json"
 
-classLinks['CNS'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27CNS%27&$format=json"
+classLinks['ECT'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27ECT%27&$expression=json"
 
-classLinks['HCI'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27HCI%27&$format=json"
+classLinks['CNS'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27CNS%27&$expression=json"
 
-classLinks['SE'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27SE%27&$format=json"
+classLinks['HCI'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27HCI%27&$expression=json"
 
-classLinks['GEO'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27GEO%27&$format=json"
+classLinks['SE'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27SE%27&$expression=json"
 
-classLinks['IPD'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27IPD%27&$format=json"
+classLinks['GEO'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27GEO%27&$expression=json"
 
-classLinks['MGT'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27MGT%27&$format=json"
+classLinks['IPD'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27IPD%27&$expression=json"
 
-classLinks['IT'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27IT%27&$format=json"
+classLinks['MGT'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27MGT%27&$expression=json"
 
-for key in classLinks:
-    classDict[key] = []
+classLinks['IT'] = "http://odata.cdm.depaul.edu/Cdm.svc/Courses?$orderby=CatalogNbr&$filter=EffStatus%20eq%20%27A%27%20and%20SubjectId%20eq%27IT%27&$expression=json"
 
 # - - - - - FUNCTIONS - - - - - #
 
@@ -75,15 +76,16 @@ def get_Degrees():
     ISConcentrations = []
     for li in soup.findAll("ul", { "class" : "sub-nav" })[0].findAll('li'):
         concentration = Concentration(li.a.text, li.a['href'], [])
-        ISConcentrations.append(concentration)
+        concentrationDict[li.a.text] = concentration
+        ISConcentrations.append(li.a.text)
     degrees.append(Degree(soup.findAll("span", { "class" : "breadcrumbCurrent" })[0].text, ISConcentrations))
     
     #CSC
     url = base + "/academics/Pages/MSInComputerScience.aspx"
     soup = get_soup_from_url(url)
-    CSCConcentrations = []
-    CSCConcentrations.append(Concentration("MS in Computer Science", "/academics/Pages/Current/Requirements-MS-in-Computer-Science.aspx", []))
-    degrees.append(Degree(soup.findAll("span", {"class": "breadcrumbCurrent"})[0].text, CSCConcentrations))
+    CSCConcentration = Concentration("MS in Computer Science", "/academics/Pages/Current/Requirements-MS-in-Computer-Science.aspx", [])
+    concentrationDict["MS in Computer Science"] = CSCConcentration
+    degrees.append(Degree(soup.findAll("span", {"class": "breadcrumbCurrent"})[0].text, ["MS in Computer Science"]))
 
     return degrees
 
@@ -92,14 +94,44 @@ def get_priority():
     return
 
 def get_prereqs(DescrLong):
-    #"PREREQUISITE(S)" are structured in varying formats in data, so currently only returns string.
+    #"PREREQUISITE(S)" are structured in varying expressions in data, so currently only returns string.
     #Will need to make a more in-depth parser to solve this.
+    prereqs = []
     if DescrLong is None:
-        return []
+        return prereqs
     if "PREREQUISITE(S)" in DescrLong:
         x = DescrLong.split("PREREQUISITE(S)")[1]
-        #print(x)
-        return x
+        try:
+            x = x.split(": ")[1].lstrip()
+        except:
+            pass
+        #'Class AND (Class or ... or Class)'
+        pattern = re.compile('[A-Z]+ [0-9]{3} (AND)|(and) \([[A-Z]+ [0-9]{3} (or)?]+\)')
+        expression = pattern.match(x)
+        if expression:
+            text = expression.group().split()
+            print text
+            prereqs.append(text[0] + ' ' + text[1])
+            count = 3
+            while count < len(text):
+                prereqs.append(text[count] + ' ' + text[count+1])
+                count+=3
+            return prereqs
+        #'Class AND Class'
+        pattern = re.compile('[A-Z]+ [0-9]{3} (AND)|(and) [A-Z]+ [0-9]{3}')
+        expression = pattern.match(x)
+        if expression:
+            text = expression.group().split()
+            print text
+            prereqs.append(text[0] + ' ' + text[1])
+            prereqs.append(text[3] + ' ' + text[4])
+            return prereqs
+        #'Class'
+        pattern = re.compile('[A-Z]+ [0-9]{3}')
+        expression = pattern.match(x)
+        if expression:
+            prereqs.append(expression.group())
+        return prereqs
 
 def get_terms(TypicallyOffered):
     #Will parse "TypicallyOffered" data.
@@ -111,12 +143,12 @@ def get_Classes(links):
         response = urllib.urlopen(links[x])
         data = json.loads(response.read())
         for y in data['d']['results']:
-            classDict[x].append(Class(y['CatalogNbr'], y["CourseTitleLong"], False, get_prereqs(y["DescrLong"]), get_terms(y["TypicallyOffered"]), y["IsInClassOnly"], y["IsOnlineOnly"], 0))
+            classDict[x + ' ' + y['CatalogNbr']] = Class(y['CatalogNbr'], y["CourseTitleLong"], False, get_prereqs(y["DescrLong"]), get_terms(y["TypicallyOffered"]), y["IsInClassOnly"], y["IsOnlineOnly"], 0, None)
 
 def put_classes():
     for x in degrees:
         for y in x.concentrations:
-            url = base + y.link
+            url = base + concentrationDict[y].link
             soup = get_soup_from_url(url)
             count = 0
             total = len(soup.findAll("table", { "class" : "courseList" }))
@@ -127,12 +159,21 @@ def put_classes():
                     for z in tr:
                         for a in z:
                             for b in a:
-                                try:
-                                    number = b.split()
-                                    found = next((c for c in classDict[number[0]] if c.code == number[1]), None)
-                                except:
-                                    pass
-                                y.classes.append(found)
+                                if b.find('OR') == -1:    
+                                    try:
+                                        b = b.rstrip()
+                                    except:
+                                        pass
+                                    if b in classDict:
+                                        concentrationDict[y].classes.append(b)
+                                else:
+                                    for c in b:
+                                        if c in classDict:
+                                            position = len(concentrationDict[y].classes)
+                                            previous = concentrationDict[y].classes[position-1]
+                                            concentrationDict[y].classes.append(c)
+                                            classDict[c].OR = previous
+                                            classDict[previous].OR = c
                 count+=1
 
 def get_soup_from_url(url):
@@ -167,11 +208,10 @@ put_classes()
 #print classLinks
 #print classDict
 
-##for x in degrees:
-##    print x.name
-##    for y in x.concentrations:
-##        print y.name
-##        print y.link
-##        for z in y.classes:
-##            print z.name
-##            print z
+for x in degrees:
+    print x.name
+    for y in x.concentrations:
+        print concentrationDict[y].name
+        for z in concentrationDict[y].classes:
+            #print classDict[z].name
+            print classDict[z].prereqs
