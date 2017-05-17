2b91d565ae43fedbd31fab434ec46c5c07940b0f
www-data <www-data@ip-172-31-26-105.us-west-2.compute.internal>
2017-05-17 23:37:12 +0000

    added students view

--- controllers/default.py
+++ controllers/default.py
@@ -14,11 +14,28 @@ def call(): return service()
 def index():
     return dict()
 
+@auth.requires_login()
 def search():
     return dict()
-
+@auth.requires_login()
 def result():
     return dict()
 
+@auth.requires_login()
+@auth.requires_membership("faculty")
+def students():
+    grid = SQLFORM.grid(db.auth_user)
+    return locals()
 def error():
     return dict()
+
+def test():
+    test = Mail()
+    test.settings.server = 'email-smtp.us-west-2.amazonaws.com:587'
+    test.settings.sender = 'sunnysyed93@gmail.com'
+    test.settings.login = 'AKIAJMDHJPWDX3XJIQ5Q:BFT48Dz12fJECfPzUJ61xZDYgYuYVseBaGO+MwCl'
+    test.settings.tls = True
+    temp = test.send('sunnysyed93@gmail.com',
+  'Message subject',
+  'Plain text body of the message')
+    return response.json(temp)
